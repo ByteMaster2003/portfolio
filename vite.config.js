6ccd45db3757434a9e822728d868f7e6e472736d
Vivek-Sahani <viveksahani39266@gmail.com>
2025-07-27 23:12:00 +0530

    refactor: Update vite configuration to output css and js files with fix names

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
 	server: {
 		port: 3000,
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: "assets/index.js",
+				assetFileNames: "assets/[name].[ext]"
+			}
+		}
+	}
 });
