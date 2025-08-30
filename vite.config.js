95fdbae5149da6910633a8105369931726ff50af
Vivek-Sahani <viveksahani39266@gmail.com>
2025-08-30 10:42:20 +0530

    refactor: remove seo directory since not required and inject SEO content in index.html file

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import fs from "fs";
-import path from "path";
-
-function replaceIndexHtmlPlugin() {
-	return {
-		name: "replace-index-html",
-		closeBundle() {
-			const distPath = path.resolve("dist/index.html");
-			const seoHtmlPath = path.resolve("seo/index.html");
-
-			if (fs.existsSync(seoHtmlPath)) {
-				fs.copyFileSync(seoHtmlPath, distPath);
-				console.log("✅ index.html replaced with SEO version");
-			} else {
-				console.warn("⚠️ SEO index.html not found!");
-			}
-		},
-	};
-}
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), replaceIndexHtmlPlugin()],
+	plugins: [react(), tailwindcss()],
 	server: {
 		port: 3000,
 	},
